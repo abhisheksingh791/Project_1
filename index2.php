55ac01fe6640f2b06c5aed67f6222da7d97c4543
Abhishek kumar singh <76807380+abhisheksingh791@users.noreply.github.com>
2021-04-27 00:25:45 +0530

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,30 @@
+<?php
+//first class
+
+if (isset($_POST["name"])){
+$var=$_POST["name"];
+}
+else{
+$var=array("tony", "steve", "self", "franklin");
+}
+
+
+
+$var3=6;
+$var2="<html>
+<head> </head>
+<body>
+    <h1>Hello from arbazkhan Pathan</h1>
+    <form action='http://localhost/lessons/index2.php' method = 'POST'>
+    <input type='text' name='name'></input><br />
+    <input type='submit' value='Submit'>
+    </form>    
+    </body>
+    </html>";  
+
+//print_r($var);
+echo $var[0];
+echo $var2;
+
+
+?>
