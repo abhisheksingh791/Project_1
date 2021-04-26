57e4484dde6d28e75d546cad574296f1d0c8aa9c
Abhishek kumar singh <76807380+abhisheksingh791@users.noreply.github.com>
2021-04-27 00:19:40 +0530

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+// if else condition is
+
+$newvar="My name is Abhishek Kumar Singh";
+$newvar1="no input found";
+$check=3;
+
+if($check==6){
+    echo $newvar;
+}
+elseif($newvar>=4){
+    echo "second result";
+}
+else{
+    echo $newvar1; 
+}
+
+
+
+// while condition is
+
+while($check<7){
+    echo $check."<br>";
+    $check++;
+}
+
+
+
+?>
